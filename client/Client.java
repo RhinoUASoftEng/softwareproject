3fac44d1be3b3cf8a538e87deec98885bf8b8cec
benwalcutt <bewalcut@email.uark.edu>
2015-09-28 17:39:23 -0500

    new server functionality

--- client/Client.java
+++ client/Client.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class Client {
 	public static void main(String args[]) {
 		int portNumber = 5000;
-		String hostName = "52.88.151.20";
+		String hostName = "localhost";
 		
 		
 		try (
@@ -15,16 +15,28 @@ public class Client {
 				BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 				String fromServer;
 				String fromUser;
-				
+				fromUser = args[0];
+				out.println(fromUser);
+				fromServer = in.readLine();
+				System.out.println(fromServer);
+				/*
 				while ((fromServer = in.readLine()) != null) {
+
+					if (fromServer.equals("done")) {
+						break;
+					}
 					System.out.println("Server: " + fromServer);
-					
+					/*
 					fromUser = stdIn.readLine();
 					if (fromUser != null) {
 						System.out.println("Client: " + fromUser);
 						out.println(fromUser);
 					}
-				}
+					/**/
+					//out.println("test");
+					
+				//}
+				
 			} catch (IOException e) {
 				System.out.println(e);
 			}
---
+++ server/start.bat
@@ -0,0 +1,1 @@
+java -cp c:\tools\postgresql-9.4-1023.jdbc4.jar; server_driver

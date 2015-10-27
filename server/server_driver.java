0258d36b180d896504479ac7f5b7673865b8f072
benwalcutt <bewalcut@email.uark.edu>
2015-10-27 12:18:42 -0500

    removing old server files

--- server/server_driver.java
+++
@@ -1,8 +0,0 @@
-public class server_driver {
-	
-	public static void main(String args[]) {
-		MainServer server = new MainServer();
-		
-		server.start();
-	}
-}
--- server/start.bat
+++
@@ -1,1 +0,0 @@
-java -cp c:\tools\postgresql-9.4-1023.jdbc4.jar; server_driver

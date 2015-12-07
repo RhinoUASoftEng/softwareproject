93a1dfd73fef41d037d3bbc74f5dadd500caad7f
benwalcutt <bewalcut@email.uark.edu>
2015-12-06 22:16:17 -0600

    Adding error handling

--- server/src/main/java/org/softeng/project/hb_server/resources/products.java
+++ server/src/main/java/org/softeng/project/hb_server/resources/products.java
@@ -3,15 +3,20 @@ package org.softeng.project.hb_server.resources;
 import java.util.List;
 import java.util.UUID;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.xml.bind.JAXBElement;
 
 import org.softeng.project.hb_server.model.product;
@@ -21,6 +26,19 @@ import org.softeng.project.hb_server.services.ProductService;
 public class products {
 	
 	ProductService productService = new ProductService();
+	
+	@GET
+	@Path("/demo")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getDemo() {
+		
+		Response response = Response.status(200)
+				.entity("{\"Name\":\"Ben\"}")
+				.header("Access-Control-Allow-Origin", "*")
+				.header("Content-Type", "application/json")
+				.build();
+		return response;
+	}
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
@@ -48,8 +66,12 @@ public class products {
 	@Path("/{productid}/{field}/{newinfo}")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String updateProduct(@PathParam("productid") UUID productID, @PathParam("field") String field, @PathParam("newinfo") String newInfo) {
-		productService.updateSingleField(productID, field, newInfo);
-		return "Success";
+		if (productService.updateSingleField(productID, field, newInfo) == true) {
+			return "Success";
+		}
+		else {
+			throw new BadRequestException();
+		}
 	}
 
 	@DELETE
